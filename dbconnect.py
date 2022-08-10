c3e904944a6b75e1072bdb091e78701077ae0916
Serena <sliu80@my.smccd.edu>
2022-08-10 11:19:04 -0700

    added db.py file

---
+++ dbconnect.py
@@ -0,0 +1,13 @@
+import pymysql
+
+
+def connection():
+    conn = pymysql.connect(host="eu-cdbr-west-01.cleardb.com",
+                           user="b23392502681a4",
+                           passwd="a421218d",
+                           db="heroku_d710c01591e6b23",
+                           connect_timeout=120,
+                           autocommit=True)
+
+    c = conn.cursor()
+    return c, conn
